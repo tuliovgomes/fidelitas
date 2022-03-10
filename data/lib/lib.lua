0ae5665e41aa1cb8d646afceced4d55acf5fb462
Túlio Gomes <tuliogomes07@hotmail.com>
2022-03-09 22:12:39 -0300

    first commit

---
+++ data/lib/lib.lua
@@ -0,0 +1,12 @@
+-- Core API functions implemented in Lua
+dofile('data/lib/core/load.lua')
+dofile('data/lib/core/functions/load.lua')
+
+-- Compatibility library for our old Lua API
+dofile('data/lib/compat/compat.lua')
+
+-- Debugging helper function for Lua developers
+dofile('data/lib/debugging/dump.lua')
+
+-- Tables library
+dofile('data/lib/tables/load.lua')
