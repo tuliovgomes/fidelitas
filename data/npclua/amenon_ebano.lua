b05b25b5063ac791562bbfb72d236a021693a47c
Túlio Gomes <tuliogomes07@hotmail.com>
2022-03-27 23:56:46 -0300

    fix itens and npcs

--- data/npclua/amenon_ebano.lua
+++ data/npclua/amenon_ebano.lua
@@ -20,12 +20,6 @@ npcConfig.outfit = {
 	lookAddons = 2,
 }
 
-npcConfig.voices = {
-	interval = 15000,
-	chance = 20,
-	{ text = "Welcome to Fidelitas Server!" }
-}
-
 npcConfig.flags = {
 	floorchange = false
 }
@@ -55,7 +49,7 @@ npcConfig.shop = {
 	{ itemName = "terra mantle", clientId = 811, sell = 20000 },
 	{ itemName = "golden armor", clientId = 3360, sell = 60000 },
 	{ itemName = "crystalline armor", clientId = 8050, sell = 41000 },
-	{ itemName = "divine plate", clientId = 8862, sell = 400000 },
+	{ itemName = "valar plate", clientId = 8862, sell = 400000 },
 	{ itemName = "dragon scale mail", clientId = 3386, sell = 40000 },
 	{ itemName = "magic plate armor", clientId = 3366, sell = 200000 },
 	{ itemName = "legolas plate", clientId = 8063, sell = 330000 },
@@ -67,81 +61,46 @@ npcConfig.shop = {
 	{ itemName = "plate armor", clientId = 3357, buy = 3650},
 }
 
--- Create keywordHandler and npcHandler
-local keywordHandler = KeywordHandler:new()
-local npcHandler = NpcHandler:new(keywordHandler)
-
--- onThink
-npcType.onThink = function(npc, interval)
-	npcHandler:onThink(npc, interval)
-end
-
--- onAppear
-npcType.onAppear = function(npc, creature)
-	npcHandler:onAppear(npc, creature)
-end
-
--- onDisappear
-npcType.onDisappear = function(npc, creature)
-	npcHandler:onDisappear(npc, creature)
-end
-
--- onMove
-npcType.onMove = function(npc, creature, fromPosition, toPosition)
-	npcHandler:onMove(npc, creature, fromPosition, toPosition)
-end
-
--- onSay
-npcType.onSay = function(npc, creature, type, message)
-	npcHandler:onSay(npc, creature, type, message)
-end
-
--- onPlayerCloseChannel
-npcType.onCloseChannel = function(npc, player)
-	npcHandler:onCloseChannel(npc, player)
-end
-
 -- On buy npc shop message
 npcType.onBuyItem = function(npc, player, itemId, subType, amount, inBackpacks, name, totalCost)
 	npc:sellItem(player, itemId, amount, subType, true, inBackpacks, 2854)
 	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Bought %ix %s for %i %s.", amount, name, totalCost, ItemType(npc:getCurrency()):getPluralName():lower()))
 end
-
 -- On sell npc shop message
 npcType.onSellItem = function(npc, player, clientId, subtype, amount, name, totalCost)
 	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Sold %ix %s for %i gold.", amount, name, totalCost))
 end
-
 -- On check npc shop message (look item)
 npcType.onCheckItem = function(npc, player, clientId, subType)
 end
 
--- Function called by the callback "npcHandler:setCallback(CALLBACK_GREET, greetCallback)" in end of file
-local function greetCallback(npc, player)
-	npcHandler:setMessage(MESSAGE_GREET, "Hello |PLAYERNAME|, how i can help you?")
-	return true
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+
+npcType.onThink = function(npc, interval)
+	npcHandler:onThink(npc, interval)
 end
 
--- On creature say callback
-local function creatureSayCallback(npc, player, type, msg)
-	local playerId = player:getId()
-	if not npcHandler:checkInteraction(npc, player) then
-		return false
-	end
-	return true
+npcType.onAppear = function(npc, creature)
+	npcHandler:onAppear(npc, creature)
 end
 
--- Set to local function "greetCallback"
-npcHandler:setCallback(CALLBACK_GREET, greetCallback)
--- Set to local function "creatureSayCallback"
-npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcType.onDisappear = function(npc, creature)
+	npcHandler:onDisappear(npc, creature)
+end
 
--- Bye message
-npcHandler:setMessage(MESSAGE_FAREWELL, "Yeah, good bye and don't come again!")
--- Walkaway message
-npcHandler:setMessage(MESSAGE_WALKAWAY, "You not have education?")
+npcType.onMove = function(npc, creature, fromPosition, toPosition)
+	npcHandler:onMove(npc, creature, fromPosition, toPosition)
+end
+
+npcType.onSay = function(npc, creature, type, message)
+	npcHandler:onSay(npc, creature, type, message)
+end
+
+npcType.onCloseChannel = function(npc, creature)
+	npcHandler:onCloseChannel(npc, creature)
+end
 
 npcHandler:addModule(FocusModule:new())
 
--- Register npc
 npcType:register(npcConfig)
--- data/npclua/mercury_hermes.lua
+++ data/npclua/mercury_hermes.lua
@@ -20,12 +20,6 @@ npcConfig.outfit = {
 	lookAddons = 2,
 }
 
-npcConfig.voices = {
-	interval = 15000,
-	chance = 20,
-	{ text = "Welcome to Fidelitas Server!" }
-}
-
 npcConfig.flags = {
 	floorchange = false
 }
@@ -50,81 +44,46 @@ npcConfig.shop = {
 	{ itemName = "boots of haste", clientId = 3079, buy = 45000},
 }
 
--- Create keywordHandler and npcHandler
-local keywordHandler = KeywordHandler:new()
-local npcHandler = NpcHandler:new(keywordHandler)
-
--- onThink
-npcType.onThink = function(npc, interval)
-	npcHandler:onThink(npc, interval)
-end
-
--- onAppear
-npcType.onAppear = function(npc, creature)
-	npcHandler:onAppear(npc, creature)
-end
-
--- onDisappear
-npcType.onDisappear = function(npc, creature)
-	npcHandler:onDisappear(npc, creature)
-end
-
--- onMove
-npcType.onMove = function(npc, creature, fromPosition, toPosition)
-	npcHandler:onMove(npc, creature, fromPosition, toPosition)
-end
-
--- onSay
-npcType.onSay = function(npc, creature, type, message)
-	npcHandler:onSay(npc, creature, type, message)
-end
-
--- onPlayerCloseChannel
-npcType.onCloseChannel = function(npc, player)
-	npcHandler:onCloseChannel(npc, player)
-end
-
 -- On buy npc shop message
 npcType.onBuyItem = function(npc, player, itemId, subType, amount, inBackpacks, name, totalCost)
 	npc:sellItem(player, itemId, amount, subType, true, inBackpacks, 2854)
 	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Bought %ix %s for %i %s.", amount, name, totalCost, ItemType(npc:getCurrency()):getPluralName():lower()))
 end
-
 -- On sell npc shop message
 npcType.onSellItem = function(npc, player, clientId, subtype, amount, name, totalCost)
 	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Sold %ix %s for %i gold.", amount, name, totalCost))
 end
-
 -- On check npc shop message (look item)
 npcType.onCheckItem = function(npc, player, clientId, subType)
 end
 
--- Function called by the callback "npcHandler:setCallback(CALLBACK_GREET, greetCallback)" in end of file
-local function greetCallback(npc, player)
-	npcHandler:setMessage(MESSAGE_GREET, "Hello |PLAYERNAME|, how i can help you?")
-	return true
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+
+npcType.onThink = function(npc, interval)
+	npcHandler:onThink(npc, interval)
 end
 
--- On creature say callback
-local function creatureSayCallback(npc, player, type, msg)
-	local playerId = player:getId()
-	if not npcHandler:checkInteraction(npc, player) then
-		return false
-	end
-	return true
+npcType.onAppear = function(npc, creature)
+	npcHandler:onAppear(npc, creature)
 end
 
--- Set to local function "greetCallback"
-npcHandler:setCallback(CALLBACK_GREET, greetCallback)
--- Set to local function "creatureSayCallback"
-npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcType.onDisappear = function(npc, creature)
+	npcHandler:onDisappear(npc, creature)
+end
 
--- Bye message
-npcHandler:setMessage(MESSAGE_FAREWELL, "Yeah, good bye and don't come again!")
--- Walkaway message
-npcHandler:setMessage(MESSAGE_WALKAWAY, "You not have education?")
+npcType.onMove = function(npc, creature, fromPosition, toPosition)
+	npcHandler:onMove(npc, creature, fromPosition, toPosition)
+end
+
+npcType.onSay = function(npc, creature, type, message)
+	npcHandler:onSay(npc, creature, type, message)
+end
+
+npcType.onCloseChannel = function(npc, creature)
+	npcHandler:onCloseChannel(npc, creature)
+end
 
 npcHandler:addModule(FocusModule:new())
 
--- Register npc
 npcType:register(npcConfig)
